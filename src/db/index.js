8e1960d715d7cb9e1260a22c24ab0b7ac76492c9
Ashish-Nauhwar <itsyankeesyogi8412@gmail.com>
2026-05-31 22:11:06 +0530

    connecting to database

---
+++ src/db/index.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+import dns from "dns";
+dns.setServers([
+    '1.1.1.1',
+    '8.8.8.8'
+])
+
+
+
+
+const connectDB = async () => {
+    console.log("MONGODB_URL =", process.env.MONGODB_URL);
+    console.log("DB_NAME =", DB_NAME);
+    try {
+        console.log("MONGODB_URL:", process.env.MONGODB_URL);
+        const connectionIntence = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`\n MongoDB connected !! DB HOST : ${connectionIntence.connection.host}`);
+        
+    } catch (error) {
+        console.log("MongoDB connection Error :",  error );
+        process.exit(1)
+        
+    }
+}
+
+export default connectDB;
