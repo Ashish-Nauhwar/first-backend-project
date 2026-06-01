afb2eb3068858bec1ad00ba2787b21e12fc278fc
Ashish-Nauhwar <itsyankeesyogi8412@gmail.com>
2026-06-01 09:33:03 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -9,6 +9,31 @@ dotenv.config({
 
 connectDB()
 
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGODB connection failed" , error);
+}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 import express from "express";
 const app = express();
