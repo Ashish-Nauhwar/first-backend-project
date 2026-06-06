9df9bf220f2e691fff63ca8842afd376ea979e33
Ashish-Nauhwar <itsyankeesyogi8412@gmail.com>
2026-06-06 23:53:20 +0530

    simplifying code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-    
-    import dotenv from "dotenv"
+    //used to load enviroment varibles from a .env file to node process.env
+    import dotenv from "dotenv"  
+
     import connectDB from "./db/index.js"
     import { app } from "./app.js"
 
-
+   // executes the configuration method for look for .env file
+   // in the root directory(./.env) so your app can read private keys
     dotenv.config({
     path: './.env'
     })
 
+    //it checks if a PORT varible is defined in your .env file otherwise defaults 8000
     const PORT = process.env.PORT || 8000
+
 
     connectDB()
     // connect to Mongodb
+    //promise handling if connectDB ✅ then run the code in .then()
     .then(() => {
     console.log("✅ MongoDB connected successfully!")
 
-    // Start the Express server ONLY after the database connects
+
+   //tells your express app to start listening for incoming HTTP request on specified PORT
     app.listen(PORT , () => {
         console.log(`server is running at port : ${process.env.PORT}`)
     })
